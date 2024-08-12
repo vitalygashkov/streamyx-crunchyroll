0355a77fd2dc5bb00993e3493f19ba372ffb0106
Vitaly Gashkov <vitalygashkov@vk.com>
2024-08-12 11:18:04 +0500

    chore: update deps & tests

--- test/crunchyroll.test.ts
+++ test/crunchyroll.test.ts
@@ -1,27 +1,26 @@
 import path from 'node:path';
 import { expect, test } from 'vitest';
-import { create } from '@streamyx/core';
-import { crunchyroll } from '../crunchyroll';
+import { registerService } from '@streamyx/core';
+import crunchyroll from '../crunchyroll';
 
 const configPath = path.join(__dirname, './config.json');
 
-test('create plugin', () => {
-  const core = create(crunchyroll.name);
-  const plugin = crunchyroll({ configPath })(core);
-  expect(plugin).toBeDefined();
-  expect(plugin).toHaveProperty('name', 'crunchyroll');
-  expect(plugin).toHaveProperty('api');
-  expect(plugin).toHaveProperty('fetchMediaInfo');
+test('register service', () => {
+  const service = registerService(crunchyroll, { configPath });
+  expect(service).toBeDefined();
+  expect(service).toHaveProperty('name', 'crunchyroll');
+  expect(service).toHaveProperty('api');
+  expect(service).toHaveProperty('fetchMediaInfo');
 });
 
 test('sign in', async () => {
   const username = '';
   const password = '';
   if (!username || !password) return; // Skip test if credentials are not provided
-  const core = create(crunchyroll.name);
-  const plugin = crunchyroll({ configPath })(core);
-  await plugin.api.auth.signIn(username, password);
-  console.log(plugin.api.auth.state);
-  expect(plugin.api.auth.state.accessToken).toBeDefined();
-  await plugin.api.auth.signOut();
+  const service = registerService(crunchyroll);
+  const api = service.api!;
+  await api.auth.signIn(username, password);
+  console.log(api.auth.state);
+  expect(api.auth.state.accessToken).toBeDefined();
+  await api.auth.signOut();
 });
